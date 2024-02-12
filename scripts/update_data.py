e1aee389560bbcc9c7b5275d4a7cd8bb2d5dff39
nathankelion <nathankelion26@gmail.com>
2024-02-12 13:05:49 +0000

    fdfd

--- scripts/update_data.py
+++ scripts/update_data.py
@@ -21,7 +21,7 @@ if 'GITHUB_ACTIONS' in os.environ:
     # Specify the path to ChromeDriver
     chrome_driver_path = '/usr/local/bin'
     # Initialize ChromeDriver with the specified path
-    driver = webdriver.Chrome(chrome_driver_path)
+    driver = webdriver.Chrome(chrome_driver_path, options=chrome_options)
 else:
     # Running locally
     download_folder = r'C:\Users\natha\OneDrive\Documents\data_science_projects\mortgage_calculator'
@@ -32,7 +32,7 @@ else:
         'safebrowsing.enabled': True
     })
     # Initialize ChromeDriver
-    driver = webdriver.Chrome(options=chrome_options, options=chrome_options)
+    driver = webdriver.Chrome(options=chrome_options)
 
 # Navigate to the zip file link
 driver.get(zip_url)
